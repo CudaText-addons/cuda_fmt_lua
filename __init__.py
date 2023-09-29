4e3cbe54926eb8797facb83df817298bfb9cd05e
Alexey <support@uvviewsoft.com>
2023-09-29 08:17:09 +0300

    fix tab_size

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 from .lua_format import lua_format
+import cudatext as app
 
 def do_format(text):
 
     settings = {
-        'tab_size': 4,
+        'tab_size': app.ed.get_prop(app.PROP_TAB_SIZE),
         'special_symbol_split': True,
         'bracket_split': False,
         }
